f42ba0d451aaf2d419f17935b4257eb8814c2454
Yasmin Andrade <yasminandrade7878@gmail.com>
2026-07-26 13:54:31 -0300

    .

--- sorveteria/pessoa/urls.py
+++ sorveteria/pessoa/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),          # Funciona em http://127.0.0.1:8000/
-    path('index/', views.home, name='index'),   # Funciona em http://127.0.0.1:8000/index/
+    path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('icecream/', views.icecream, name='icecream'),
     path('services/', views.services, name='services'),
