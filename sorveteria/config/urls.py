92aae6c4e76f78bb79d6e35747110f76e8fe44b5
Francielly Souza <francielly1414@gmail.com>
2026-09-22 00:56:37 -0300

    implementação do frameork nas partes de listar

--- sorveteria/config/urls.py
+++ sorveteria/config/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('sobre/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('contato/', TemplateView.as_view(template_name='contact.html'), name='contact'),
     
     # Rotas dos seus aplicativos:
     path('pessoas/', include('pessoa.urls')),
